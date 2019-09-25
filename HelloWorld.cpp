be29ecddc2a696395ea59504cdcf1dca81d741a8
Joaby Oliveira <bigmantrets@gmail.com>
2019-09-25 12:15:23 -0300

    Aula dia 25/09/2019

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+# include<stdio.h>
+#include<window.h>
+int main(){
+  	setlocale(LC_ALL, "");
+		int n1, n2, n3, media = 0;
+			printf("Digite um numero");
+			scanf(%i);
+			printf("Digite outro numero");
+			scanf(%i);
+			printf("Digite mais um numero");
+			scanf(%i)
+		media = (n1 + n2 + n3) / 2;
+}
